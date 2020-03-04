2e7d3ae4362dbbb2deae461749d6b0adb7d90948
Apoorva Mishra <amishra@zipcodes-MacBook-Pro.local>
2020-03-04 14:26:08 -0500

    exercise 2

---
+++ greet.py
@@ -0,0 +1,11 @@
+def greet(name):
+    print(name)
+    
+greet("Apoorva")
+greet("Darth Vader")
+greet("Guido van Rossum")
+def name_input():
+    name = input("Enter the name\n")
+    print(name)
+name_input() 
+
---
+++ square.py
@@ -0,0 +1,6 @@
+square = lambda x : exponentiate(x, 2)
+cube = lambda y : exponentiate(y, 3)
+
+print(square(2))
+print(cube(2))
+print(raise_to_fourth_power(2))
