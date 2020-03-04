2f998bb711945be1b4216d8563d799823a4d1d69
Apoorva Mishra <apoorvashukla88@gmail.com>
2020-03-04 14:30:57 -0500

    expo file

---
+++ exponentiate.py
@@ -0,0 +1,11 @@
+def exponentiate(num1, num2):
+    """returns the power of number"""
+    result = pow(num1, num2)
+    return result
+    
+exponentiate(4,5) 
+def raise_to_fourth_power(num3):
+    result_fourth_power = exponentiate(num3,4)
+    return result_fourth_power
+    
+raise_to_fourth_power(4)  
